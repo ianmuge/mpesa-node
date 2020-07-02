f7a54403dbebad9d9ce0593e2d828a0a246d8b80
Ian Muge <imuge94@gmail.com>
2020-07-02 20:04:17 +0300

    added transaction view page

--- routes/transaction/index.js
+++ routes/transaction/index.js
@@ -1,15 +1,22 @@
 let route = require('express').Router();
 const { eventEmitter } = require("../../events");
 const Transaction=require("../../models/transaction");
-route.get('/', async (req, res, next) => {
-    try {
-        let data= {
-            answer: 42
-        };
-        eventEmitter.emit('test-created', data);
-        res.status(200).json(data);
-    } catch (error) {
-        next(error)
-    }
+route.get('/:page?', async (req, res, next) => {
+    var perPage = 25;
+    var page = req.params.page || 1;
+    Transaction
+        .find({})
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec((err, transactions) =>{
+            Transaction.countDocuments().exec(function(err, count) {
+                if (err) return next(err);
+                res.render('transaction/index', {
+                    transactions:transactions,
+                    current:page,
+                    pages:Math.ceil(count / perPage)
+                });
+            });
+        });
 });
 module.exports = route;
